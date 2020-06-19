c03fa7afd301ad88d1fe5be4061b1b436e9ef273
Aman <amangrand@gmail.com>
2020-06-19 20:14:52 +0530

    Spiral Traversal

---
+++ Binary Tree/3. Spiral Order Traversal.cpp
@@ -0,0 +1,65 @@
+
+/*
+Date: 13-06-2020
+Author: Aman Jain
+
+Problem : Spiral  order traversal (Recursive)
+Question Link :  https://practice.geeksforgeeks.org/problems/level-order-traversal-in-spiral-form/1
+
+
+(Solution to Geeks for geeks)
+
+*/
+
+Method 1: Using two stacks
+
+
+void printSpiral(Node *root)
+{
+     stack<Node *> left;
+     stack<Node *> right;
+     
+     if(!root)
+     return ;
+     
+     cout<<root->data<<" ";
+     if(root->right)
+       left.push(root->right);
+     
+     if (root->left)
+       left.push(root->left);
+ 
+     
+     while(!left.empty() || !right.empty()){
+         
+         if(!left.empty()){
+             while(!left.empty()){
+                 Node *curr=left.top();
+                 left.pop();
+                 cout<<curr->data<<" ";
+                
+                 if(curr->left)
+                  right.push(curr->left);
+                  
+                       if(curr->right)
+                   right.push(curr->right);
+                  
+           
+             }
+         }else{
+             while(!right.empty()){
+                Node *curr=right.top();
+                 right.pop();
+                 cout<<curr->data<<" ";
+                 if(curr->right)
+                   left.push(curr->right);
+                
+                 if(curr->left)
+                  left.push(curr->left);
+                  
+                
+             }
+         }
+        }
+   }
+
