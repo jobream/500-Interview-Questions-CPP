b0f1f2b37e452d68084438529743aba4068c3154
Aman <amangrand@gmail.com>
2020-06-30 18:27:53 +0530

    Invert aplhabet's case

---
+++ Bit Manipulation/15. Invert alphabet's case.cpp
@@ -0,0 +1,25 @@
+/*
+Name : Aman Jain
+Date : 30-6-2020
+
+we can easily convert an alphabets case by taking its XOR with a space
+
+*/
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    char c;
+    cin>>c;
+    c= c ^ ' ';
+    cout<<char(c);
+}
+
+
+
+
+
