4f986a6af11e4b624e7961bc7b400327abcf615e
Aman <amangrand@gmail.com>
2020-06-30 18:20:43 +0530

    Convert lowercase  character to uppercase

---
+++ Bit Manipulation/14. Convert lowercase character to uppercase.cpp
@@ -0,0 +1,26 @@
+/*
+Name : Aman Jain
+Date : 30-6-2020
+
+we can easily convert an lowercase character to corresponding uppercase character by taking its AND with a _.
+
+
+*/
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    char c;
+    cin>>c;
+    c= c & '_';
+    cout<<char(c);
+}
+
+
+
+
+
